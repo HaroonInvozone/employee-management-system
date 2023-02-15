fbadded79f615883737f4d23dd0b03aa5f536a51
M.Haroon Ali <haroon.ali@invozone.com>
2023-02-15 20:19:32 +0500

    remove commented code

--- EmployeeManagmentSystem/Controllers/EmployeeController.cs
+++ EmployeeManagmentSystem/Controllers/EmployeeController.cs
@@ -9,19 +9,16 @@ namespace EmployeeManagmentSystem.Controllers
 {
     public class EmployeeController : Controller
     {
-        //private readonly EmployeeDbContext _context;
         private readonly IEmployeeService _employeeService;
 
         public EmployeeController(IEmployeeService employeeService)
         {
-            //_context = context;
             _employeeService = employeeService;
         }
 
         // GET: Employee
         public async Task<IActionResult> Index()
         {
-            //var x = await _context.Employees.ToListAsync();
             var x = await _employeeService.GetAllEmploy();
             return View(x);
         }
@@ -40,8 +37,6 @@ namespace EmployeeManagmentSystem.Controllers
             if (ModelState.IsValid)
             {
                 await _employeeService.CreateEmploy(employee);
-                //_context.Add(employee);
-                //await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(employee);
@@ -54,8 +49,6 @@ namespace EmployeeManagmentSystem.Controllers
             {
                 return NotFound();
             }
-
-            //var employee = await _context.Employees.FindAsync(id);
             var employee = await _employeeService.GetEmpById(id);
             if (employee == null)
             {
@@ -79,19 +72,9 @@ namespace EmployeeManagmentSystem.Controllers
                 try
                 {
                     await _employeeService.EditEmploy(employee);
-                    //_context.Update(employee);
-                    //await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    //if (!EmployeeExists(employee.EmployeeId))
-                    //{
-                    //    return NotFound();
-                    //}
-                    //else
-                    //{
-                    //    throw;
-                    //}
                     throw ;
                 }
                 return RedirectToAction(nameof(Index));
@@ -107,29 +90,9 @@ namespace EmployeeManagmentSystem.Controllers
             }
 
             await _employeeService.DeleteEmploy(id);
-            //if (employee == null)
-            //{
-            //    return NotFound();
-            //}
-
             return RedirectToAction(nameof(Index));
         }
-
-        // POST: Employee/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var employee = await _context.Employees.FindAsync(id);
-        //    _context.Employees.Remove(employee);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
-        //private bool EmployeeExists(int id)
-        //{
-        //    return _context.Employees.Any(e => e.EmployeeId == id);
-        //}
+        
     }
 
 }
